c640845c60a8ca8108d44692ac3138c455f6b5b3
Arne Rubehn <arne@rubehn.com>
2026-08-17 14:07:01 +0200

    update

---
+++ visualize.py
@@ -0,0 +1,62 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+# the 25 most common consonants and 20 most common vowels, according to phoible
+core_consonants = ["p", "b", "m", "w", "f", "v", "t", "t̪", "d", "s", "z", "r", "ɾ", "l",
+                   "n", "ʃ", "tʃ", "dʒ", "j", "ɲ", "k", "g", "ŋ", "ʔ", "h"]
+core_vowels = ["i", "iː", "ĩ", "ɪ", "e", "eː", "ɛ", "a", "aː", "ã",
+               "ɨ", "ə", "u", "uː", "ũ", "ʊ", "o", "oː", "õ", "ɔ"]
+german_sounds = ["m", "j", "a", "n", "l", "s", "b", "ŋ", "g", "h", "f", "tʃ", "ɛ", "ʃ", "ɔ", "iː", "aː",
+                 "z", "uː", "dʒ", "v", "ə", "ts", "eː", "oː", "kʰ", "pʰ", "ʒ", "ɪ", "ʊ", "ɛː", "ç", "œ",
+                 "yː", "øː", "pf", "ʏ", "ʀ", "tʰ", "d"]
+
+
+def plot(func):
+    def inner(*args):
+        plt.cla()
+        plt.clf()
+        func(*args)
+    return inner
+
+
+@plot
+#def cos_similarity_heatmap(vectors, sounds, name):
+#    sim_matrix = cosine_similarity(vectors)
+#    heatmap(sim_matrix, xticklabels=sounds, yticklabels=sounds)
+#    plt.savefig(name)
+
+
+@plot
+def plot_pca(vectors, sounds, name):
+    pca = PCA(n_components=2)
+    res = pca.fit_transform(vectors)
+    plt.scatter(*np.swapaxes(res, 0, 1))
+    for sound, coordinates in zip(sounds, res):
+        plt.annotate(sound, coordinates)
+    plt.savefig(name)
+
+
+@plot
+def plot_tsne(vectors, sounds, name):
+    tsne = TSNE(n_components=2, perplexity=5)
+    res = tsne.fit_transform(np.array(vectors))
+    plt.scatter(*np.swapaxes(res, 0, 1))
+    for sound, coordinates in zip(sounds, res):
+        plt.annotate(sound, coordinates)
+    plt.savefig(name)
+
+
+if __name__ == "__main__":
+    embeddings = json.load(open("embeddings.json"))
+    for sample, name in [
+            (core_vowels, "corev"),
+            (core_consonants, "corec"),
+            (core_vowels + core_consonants, "corecv"),
+            (german_sounds, "german")]:
+        vecs = [embeddings[x] for x in sample]
+        plot_pca(vecs, sample, name + "-pca.pdf")
+        plot_tsne(vecs, sample, name + "-tsne.pdf")
